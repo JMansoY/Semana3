1404827500e8f0de5cfc3bbaf69bc3d4aef5b9bd
JMansoY <jmanso@formatelco.com>
2017-08-18 23:31:24 +0200

    Insert comment

--- Cache_Matrix.R
+++ Cache_Matrix.R
@@ -1,25 +1,27 @@
-makeCacheMatrix <- function(x = nuemeric()) {
-    m <- NULL
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL # Limpia el valor de cache
     setmatrix <- function(y) {
-        x <<- y
+        x <<- y # Cambia la matriz x por y
         m <<- NULL
     }
     getfunction <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function() m
+    setsolve <- function(solve) m <<- solve # Invierte la matriz
+    getsolve <- function() m # Si no existe la matriz, devuelve NULL, si existe 
+                             # devuelve la inversa
+    # Crea la lista con las 4 funciones
     list(ConfigMatrix = setmatrix, FuncionMatrix = getfunction,
          setsolve = setsolve,
          getsolve = getsolve)
 }
 
-cacheSolve <- function(x, ...) {
-    m <- x$getsolve()
-    if(!is.null(m)) {
+cacheSolve <- function(x, ...) {  # Funcion que calcula la inversa
+    m <- x$getsolve() #Cequea si existe la matriz
+    if(!is.null(m)) { # si no existe devuelve el mensaje
         message("getting cached data")
         return(m)
     }
     data <- x$FuncionMatrix()
-    m <- solve(data, ...) %*% data
-    x$setsolve(m)
-    m
+    m <- solve(data, ...) %*% data #Calcula la inversa de la matriz
+    x$setsolve(m) #Guarda la inversa en cache
+    m # retorna la inversa de la matriz
 }
